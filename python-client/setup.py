b582b97617a41fca9c24f19b0b52ecf51805a0a9
Kevin Hannegan <kevin@chart-a-lot.com>
2019-08-22 23:04:39 +0000

    Automated patch update, version 0.2.11 from presalytics codegen

--- python-client/setup.py
+++ python-client/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "Presalytics API"
-VERSION = "0.2.10"
+VERSION = "0.2.11"
 # To install the library, run the following
 #
 # python setup.py install
