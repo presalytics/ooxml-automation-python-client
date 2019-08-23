8169365f6cab4c99090ed9da2f5cfb08059cab2c
Kevin Hannegan <kevin@chart-a-lot.com>
2019-08-23 00:38:11 +0000

    Automated patch update, version 0.2.20 from presalytics codegen

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "presalytics_ooxml_automation"
-VERSION = "0.2.18"
+VERSION = "0.2.20"
 # To install the library, run the following
 #
 # python setup.py install
