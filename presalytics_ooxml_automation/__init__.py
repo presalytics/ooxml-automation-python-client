3001731a8102496be52a542ac1b6554491146aaa
Kevin Hannegan <kevin@chart-a-lot.com>
2019-08-23 00:21:56 +0000

    Automated patch update, version 0.2.18 from presalytics codegen

--- presalytics_ooxml_automation/__init__.py
+++ presalytics_ooxml_automation/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.2.17"
+__version__ = "0.2.18"
 
 # import apis into sdk package
 from presalytics_ooxml_automation.api.default_api import DefaultApi
