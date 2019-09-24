7cbc598cacbb77604be0a0874310196723904334
Kevin Hannegan <kevin@chart-a-lot.com>
2019-09-24 02:25:54 +0000

    Automated patch update, version 0.2.27 from presalytics codegen

--- presalytics_ooxml_automation/__init__.py
+++ presalytics_ooxml_automation/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.2.25"
+__version__ = "0.2.27"
 
 # import apis into sdk package
 from presalytics_ooxml_automation.api.default_api import DefaultApi
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "presalytics_ooxml_automation"
-VERSION = "0.2.25"
+VERSION = "0.2.27"
 # To install the library, run the following
 #
 # python setup.py install
